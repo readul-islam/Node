3675244e19f327a26245742a0af30d599af4a831
readul-islam <developer.readul@gamil.com>
2025-07-06 19:56:29 +0600

    working with application

--- File/application/app.js
+++ File/application/app.js
@@ -8,19 +8,26 @@
 
 const fs = require("fs/promises");
 /* Promise Version */
+// open (31) file descriptor or a unique id to open this file called descriptor
+// read or write
 (async () => {
   try {
+    const commandFileHandler = await fs.open("command.txt", "r");
+
     const watcher = fs.watch("command.txt");
     //   async iterator
 
-    for await (const event of watcher){
-   if(event.eventType === "change"){
-    console.log('File changed:', event.filename);
-   }
+    for await (const event of watcher) {
+      if (event.eventType === "change") {
+        // get file size in bytes
+        const stat = await commandFileHandler.stat();
+        
+        // we want to read the content
+        const content = await commandFileHandler.read(Buffer.alloc(stat.size));
+        console.log(content);
+      }
     }
   } catch (error) {
     console.log("Error:", error);
   }
 })();
-
-
