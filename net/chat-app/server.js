a06bf2778bf5ce85ac26ac4d3bcf5c9f44642a47
readul-islam <developer.readul@gamil.com>
2025-08-20 22:16:57 +0600

    chart application terminal desing

--- net/chat-app/server.js
+++ net/chat-app/server.js
@@ -15,19 +15,31 @@ server.on("connection", (socket) => {
   );
 
   const clientId = clients.length + 1;
+
+  clients.map((client) => {
+    // send the new client its ID
+    client.socket.write(`User ${client.id} is joined!`);
+  });
+
+  // send the client its ID
   socket.write(`@User_id: ${clientId}\n`);
 
-
   socket.on("data", (data) => {
-    const dataString = data.toString('utf-8');
-    const id = dataString.substring(0, dataString.indexOf('-'));
-    const message = dataString.substring(dataString.indexOf('-message') + 9);
+    const dataString = data.toString("utf-8");
+    const id = dataString.substring(0, dataString.indexOf("-"));
+    const message = dataString.substring(dataString.indexOf("-message") + 9);
     clients.map((client) => {
       client.socket.write(`User ${id}: ${message}`);
     });
+
+    socket.on("end", () => {
+      clients.map((client) => {
+        client.socket.write(`User ${client.id}  left!`);
+      });
+    });
   });
 
-  clients.push({id: clientId, socket:socket});
+  clients.push({ id: clientId, socket: socket });
 });
 
 server.listen(3000, "127.0.0.1", () => {
